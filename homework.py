0e991edaa703653d248acbdc98dcfb3b5c89d01d
VitaGor44 <95421322+VitaGor44@users.noreply.github.com>
2022-09-15 16:01:57 +0700

    Return dataclass

--- homework.py
+++ homework.py
@@ -1,28 +1,27 @@
 from typing import List, Dict
+from dataclasses import dataclass
+from dataclasses import asdict
 
 
-class InfoMessage:
+@dataclass
+class InfoMessage():
     """Информационное сообщение о тренировке."""
-
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    MESSAGE: str = (
+        "Тип тренировки: {training_type};"
+        "Длительность: {duration:.3f} ч.;"
+        "Дистанция: {distance:.3f} км;"
+        "Ср.скорость: {speed:.3f} км / ч;"
+        "Потрачено ккал: {calories:.3f}."
+    )
 
     def get_message(self) -> str:
         """Возвращает строку сообщения о тренировке"""
-        return (f"Тип тренировки: {self.training_type};"
-                f"Длительность: {self.duration:.3f} ч.;"
-                f"Дистанция: {self.distance:.3f} км;"
-                f"Ср.скорость: {self.speed:.3f} км / ч;"
-                f"Потрачено ккал: {self.calories:.3f}.")
+        return self.MESSAGE.format(**asdict(self))
 
 
 class Training:
