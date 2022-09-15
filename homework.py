f5c01632f65ca3dfd7089864f3bc67e23e28e70d
VitaGor44 <95421322+VitaGor44@users.noreply.github.com>
2022-09-15 18:18:08 +0700

    Sh** commit in message

--- homework.py
+++ homework.py
@@ -15,7 +15,7 @@ class InfoMessage():
         "Тип тренировки: {training_type}; "
         "Длительность: {duration:.3f} ч.; "
         "Дистанция: {distance:.3f} км; "
-        "Ср.скорость: {speed:.3f} км / ч; "
+        "Ср. скорость: {speed:.3f} км/ч; "
         "Потрачено ккал: {calories:.3f}."
     )
 
